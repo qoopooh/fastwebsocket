5a9e3d83271089220fa4cbe41b5cf139a1cd5356
Berm NUC <berm@simple-different.com>
2023-07-19 11:09:08 +0700

    Create a service file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Example of FastAPI with WebSocket"""
 from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -5,18 +6,19 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def index(request: Request):
+    """Index file"""
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
+    """WebSocket"""
     await websocket.accept()
     while True:
         data = await websocket.receive_text()
